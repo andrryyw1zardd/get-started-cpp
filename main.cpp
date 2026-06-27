23f013d63ad3f09cb8f4ef97957e73583ea67c98
Andrey <kaspersky1236@gmail.com>
2026-06-28 00:14:48 +0400

    oh ok

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using std::cout, std::endl;
+
+int main(int argc, char** argv) {
+  cout << "Get Started" << endl;
+
+  return 0;
+}
+
+// cmake -B build -G Ninja
+// cmake --build build
